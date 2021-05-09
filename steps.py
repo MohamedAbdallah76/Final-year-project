8fc30df804407bbeff25af2d99e664b708218e5a
MohamedAbdallah76 <82716737+MohamedAbdallah76@users.noreply.github.com>
2021-05-09 14:04:23 +0100

    Add files via upload

---
+++ steps.py
@@ -0,0 +1,24 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+class Steps:
+
+    def tokenize(data):
+        n_data = word_tokenize(data)
+
+        return n_data
+
+    def remove_stop_words(data):
+        stop_w = set(stopwords.words('english'))
+        words_to_remove = [i for i in stop_w]
+        n_data = [i for i in data if i not in words_to_remove]
+
+        return n_data
+
+    def pos_tagging(data):
+        n_data = nltk.pos_tag(data)
+
+        return n_data
+
